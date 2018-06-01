32398f5809e942542e4e3d3843527f6cf566d441
Weber <xswzaq44321@gmail.com>
2018-06-01 20:02:39 +0800

    update gauss-jordan-elimination

--- gauss_jordan_elimination.m
+++ gauss_jordan_elimination.m
@@ -1,45 +1,38 @@
 function [outputArg] = gauss_jordan_elimination(inputArg)
-[m, n] = size(inputArg);
+[m, ~] = size(inputArg);
 %gauss elimination
 for i=1:m
     if(inputArg(i, i) == 0)
-        found = 0;
         for j = i + 1:m
-            if(inputArg(j, i) ~= 0) % guaranteed to not fall into inf loop
+            if(inputArg(j, i) ~= 0)
                 inputArg = swapRow(inputArg, i, j);
-                found = 1;
-%                 fprintf(1, 'initiate swap process, swap[%d %d]\n', i, j);
+                fprintf(1, 'swap[%d, %d]\n', i, j);
                 break;
             end
         end
-        if(~found)
-            continue;
-        end
     end
-    for j = i + 1:m
-        a = inputArg(j, i) / inputArg(i, i);
-        for k = i:n
-            inputArg(j, k) = inputArg(j, k) - a * inputArg(i, k);
+    if(inputArg(i, i) ~= 0)
+        for j = i + 1:m
+            a = inputArg(j, i) / inputArg(i, i);
+            inputArg(j, :) = inputArg(j, :) - a * inputArg(i, :);
         end
     end
 end
 %jordan eliminitaion
 for i=m:-1:1
-    for j = i - 1:-1:1
-        a = inputArg(j, i) / inputArg(i, i);
-        for k = i:n
-            inputArg(j, k) = inputArg(j, k) - a * inputArg(i, k);
+    if(inputArg(i, i) ~= 0)
+        for j = i - 1:-1:1
+            a = inputArg(j, i) / inputArg(i, i);
+            inputArg(j, :) = inputArg(j, :) - a * inputArg(i, :);
         end
     end
 end
 %organizing numbers
 for i = 1:m
     a = inputArg(i, i);
-    if(a == 0)
-        continue;
+    if(a ~= 0)
+        inputArg(i, :) = inputArg(i, :) / a;
     end
-    inputArg(i, i) = inputArg(i, i) / a;
-    inputArg(i, n) = inputArg(i, n) / a;
 end
-outputArg = inputArg(:, n);
+outputArg = inputArg;
 end
--- swapRow.m
+++ swapRow.m
@@ -1,6 +1,6 @@
 function [outputArg] = swapRow(inputArg, r1, r2)
-temp = inputArg(r1, :);
-inputArg(r1, :) = inputArg(r2, :);
-inputArg(r2, :) = temp;
+inputArg(r1, :) = inputArg(r1, :) + inputArg(r2, :);
+inputArg(r2, :) = inputArg(r1, :) - inputArg(r2, :);
+inputArg(r1, :) = inputArg(r1, :) - inputArg(r2, :);
 outputArg = inputArg;
 end
